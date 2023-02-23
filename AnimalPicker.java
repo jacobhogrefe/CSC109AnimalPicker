9dbd8950d8e7d56863af28aa030e4606959e30e5
jacobhogrefe <jhogrefe03@gmail.com>
2023-02-23 13:09:55 -0500

    slight modification

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -4,8 +4,8 @@ public class AnimalPicker {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean validAnimal = false;
-        System.out.println("Which animal would you like to see? A dog or cat");
         while (!validAnimal) {
+            System.out.println("Which animal would you like to see? A dog or cat");
             String animal = input.nextLine();
             if (animal.equalsIgnoreCase("dog")) {
                 System.out.println("ASCII_DOG_ART_GOES_HERE");
