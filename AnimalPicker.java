d23a3216873658ced656f8f62a8d05a572796183
jacobhogrefe <jhogrefe03@gmail.com>
2023-02-23 13:08:35 -0500

    added continual check for correct input

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class AnimalPicker {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean validAnimal = false;
         System.out.println("Which animal would you like to see? A dog or cat");
-        String animal = input.nextLine();
-        if (animal.equalsIgnoreCase("dog")) {
-            System.out.println("ASCII_DOG_ART_GOES_HERE");
-        } else if (animal.equalsIgnoreCase("cat")) {
-            System.out.println("ASCII_CAT_GOES_HERE");
-        } else {
-            System.out.println("Invalid selection!");
+        while (!validAnimal) {
+            String animal = input.nextLine();
+            if (animal.equalsIgnoreCase("dog")) {
+                System.out.println("ASCII_DOG_ART_GOES_HERE");
+                validAnimal = true;
+            } else if (animal.equalsIgnoreCase("cat")) {
+                System.out.println("ASCII_CAT_GOES_HERE");
+                validAnimal = true;
+            } else {
+                System.out.println("Invalid selection! Please try again.");
+            }
         }
         input.close();
     }
